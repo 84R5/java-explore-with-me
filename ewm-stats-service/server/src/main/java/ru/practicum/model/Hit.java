a67aece0670d174272b625a0e0508476a52ddcbe
84R5 <o.u.n@yandex.ru>
2023-07-14 20:37:10 +0300

    <fix> change param

--- ewm-stats-service/server/src/main/java/ru/practicum/model/Hit.java
+++ ewm-stats-service/server/src/main/java/ru/practicum/model/Hit.java
@@ -1,5 +1,6 @@
 package ru.practicum.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,5 +23,6 @@ public class Hit {
     private String app;
     private String uri;
     private String ip;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
