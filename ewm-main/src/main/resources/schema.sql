21fe86337b3dc303ed3985f62131d9760ca6706f
84R5 <o.u.n@yandex.ru>
2023-07-14 23:41:50 +0300

    <fix>

--- ewm-main/src/main/resources/schema.sql
+++ ewm-main/src/main/resources/schema.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS events
     initiator_id        BIGINT                                  NOT NULL,
     locations_id        BIGINT                                  NOT NULL,
     paid                BOOL                                    NOT NULL,
-    participant_limit   BIGINT,
+    participant_limit   INTEGER,
     published_on        TIMESTAMP WITHOUT TIME ZONE,
     request_moderation  BOOL,
     state               VARCHAR(32)                             NOT NULL,
