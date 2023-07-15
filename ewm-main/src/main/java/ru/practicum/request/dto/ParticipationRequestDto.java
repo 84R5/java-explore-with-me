6af6cc1679b288febfaa1dc874e1980dacdd4c55
84R5 <o.u.n@yandex.ru>
2023-07-15 10:50:15 +0300

    <fix> change name objects

--- ewm-main/src/main/java/ru/practicum/request/dto/ParticipationRequestDto.java
+++ ewm-main/src/main/java/ru/practicum/request/dto/ParticipationRequestDto.java
@@ -1,6 +1,8 @@
 package ru.practicum.request.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.request.enums.RequestStatus;
@@ -10,19 +12,19 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @Builder
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ParticipationRequestDto {
-
+    Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime created;
 
     Long event;
 
-    Long id;
-
     Long requester;
-
     RequestStatus status;
 }
